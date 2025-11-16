ef308acb69c967e9a63d802fdfc8a27e3add5858
Anna L.D. Latour <anna@fake.com>
2025-11-16 15:11:05 +0100

    Updating talk locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "SIGAlgo Symposium 2024: De Zwarte Doos, TU Eindhoven, Eindhoven, The Netherlands",
+    51.44632866129985, 
+    5.484172496917072
+  ],
+  [
     "ModRef 2023 at CP 2023: Heart House Building, St. George Campus, University of Toronto, Toronto, Canada",
     43.6640177620086,
     -79.3943227993122
